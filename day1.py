d17689a3da4090c6f465bcc9bbc98b479919be6e
daniel.strallhofer2 <daniel.strallhofer2@klarna.com>
2022-12-03 21:48:45 +0100

    day1,2,3 complete

---
+++ day1.py
@@ -0,0 +1,14 @@
+from aocd import data
+
+elves_rations = data.split('\n\n')
+calories_sums = []
+
+for ration in elves_rations:
+    food_items = ration.split('\n')
+    food_items = [int(i) for i in food_items]
+    calories = sum(food_items)
+    calories_sums.append(calories)
+
+calories_sums.sort()
+print(calories_sums[-1])
+print(sum(calories_sums[-3:]))
---
+++ day3.py
@@ -0,0 +1,29 @@
+from aocd import lines
+
+def part1():
+    sum_priority = 0
+    for line in lines:
+        i = int(len(line)/2)
+        l1 = line[:i]
+        l2 = line[i:]
+        dup = ''.join(set([x for x in l1 if x in l2]))
+        sum_priority += get_priority(dup)
+    print(sum_priority)
+
+def part2():
+    sum_priority = 0
+    for group in range(0,len(lines),3):
+        dup1 = ''.join(set([x for x in lines[group] if x in lines[group+1]]))
+        dup2 = ''.join(set([x for x in dup1 if x in lines[group+2]]))
+        sum_priority += get_priority(dup2)
+    print(sum_priority)
+
+def get_priority(c):
+    return ord(c) - ord('a') + 1 if c.islower() else ord(c) - ord('A') + 27
+
+def main():
+    part1()
+    part2()
+
+if __name__ == '__main__':
+    main()
