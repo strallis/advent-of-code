bc5ee943dbfcbd3448907cea3f6d4bdac0261aa4
daniel.strallhofer2 <daniel.strallhofer2@klarna.com>
2022-12-25 21:14:31 +0100

    day10 part 1 complete

---
+++ day10.py
@@ -0,0 +1,184 @@
+from aocd import lines
+
+test_input = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+lines = test_input.split("\n")
+
+
+def part1() -> int:
+    x = 1
+    cycle = 0
+    ans = 0
+    for line in lines:
+
+        cycle += 1
+        if cycle in [20, 60, 100, 140, 180, 220]:
+            ans += cycle * x
+
+        if "addx" in line:
+            cycle += 1
+            if cycle in [20, 60, 100, 140, 180, 220]:
+                ans += cycle * x
+
+            v = int(line.split(" ")[1])
+            x += v
+    return ans
+
+
+def part2():
+    pass
+
+
+def main():
+    ans1 = part1()
+    # ans2 = part2()
+    print(f"part 1: {ans1}")
+
+
+if __name__ == "__main__":
+    main()
